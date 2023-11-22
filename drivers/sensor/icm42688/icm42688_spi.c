8fb2bb4cf5742dc0f0d1b6a1d8c63ccadadf3875
sradcliffe-nottinghamspirk <127249079+sradcliffe-nottinghamspirk@users.noreply.github.com>
2023-11-22 08:51:47 -0500

    drivers: sensor: icm42688: automatic bank switching

--- drivers/sensor/icm42688/icm42688_spi.c
+++ drivers/sensor/icm42688/icm42688_spi.c
@@ -8,6 +8,8 @@
 #include "icm42688_spi.h"
 #include "icm42688_reg.h"
 
+static uint8_t bank_select = 0;
+
 static inline int spi_write_register(const struct spi_dt_spec *bus, uint8_t reg, uint8_t data)
 {
 	const struct spi_buf buf[2] = {
@@ -63,9 +65,33 @@ static inline int spi_read_register(const struct spi_dt_spec *bus, uint8_t reg, 
 	return spi_transceive_dt(bus, &tx, &rx);
 }
 
+static int inline icm42688_spi_bank_select(const struct spi_dt_spec *bus, uint16_t reg)
+{
+	int res = 0;
+	uint8_t address = FIELD_GET(REG_ADDRESS_MASK, reg);
+	uint8_t bank = FIELD_GET(REG_BANK_MASK, reg);
+
+	if (address != REG_BANK_SEL) {
+		if (bank != bank_select) {
+			res = spi_write_register(bus, REG_BANK_SEL, bank);
+			if (res < 0) {
+				return res;
+			}
+			bank_select = bank;
+		}
+	}
+	return res;
+}
+
 int icm42688_spi_read(const struct spi_dt_spec *bus, uint16_t reg, uint8_t *data, size_t len)
 {
 	int res = 0;
+
+	res = icm42688_spi_bank_select(bus, reg);
+	if (res < 0) {
+		return res;
+	}
+
 	uint8_t address = FIELD_GET(REG_ADDRESS_MASK, reg);
 
 	res = spi_read_register(bus, address, data, len);
@@ -76,9 +102,10 @@ int icm42688_spi_read(const struct spi_dt_spec *bus, uint16_t reg, uint8_t *data
 int icm42688_spi_update_register(const struct spi_dt_spec *bus, uint16_t reg, uint8_t mask,
 				 uint8_t data)
 {
+	int res;
 	uint8_t temp = 0;
-	int res = icm42688_spi_read(bus, reg, &temp, 1);
 
+	res = icm42688_spi_read(bus, reg, &temp, 1);
 	if (res) {
 		return res;
 	}
@@ -94,7 +121,21 @@ int icm42688_spi_single_write(const struct spi_dt_spec *bus, uint16_t reg, uint8
 	int res = 0;
 	uint8_t address = FIELD_GET(REG_ADDRESS_MASK, reg);
 
-	res = spi_write_register(bus, address, data);
+	res = icm42688_spi_bank_select(bus, reg);
+	if (res) {
+		return res;
+	}
 
+	res = spi_write_register(bus, address, data);
+	if (res) {
+		return res;
+	}
+
+	/* These writes can change the bank select register outside our control */
+	if (address == REG_BANK_SEL) {
+		bank_select = data;
+	} else if (address == REG_DEVICE_CONFIG && FIELD_GET(BIT_SOFT_RESET, data)) {
+		bank_select = 0;
+	}
 	return res;
 }
